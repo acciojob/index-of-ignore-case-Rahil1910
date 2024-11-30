0883d0cb2a2c310a78783b2a471e2f380feb0f80
Rahil1910 <rahilrafique1234@gmail.com>
2024-11-30 14:21:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	let str=s2[0];
+	let foundword=false;
+	for(let i=0;i<s1.length;i++){
+		if(s1[i]===str){
+			foundword=true;
+			return i;
+		}
+	}
+	if(foundword===false){
+		return -1;
+	}
 }
-
 // Please do not change the code below
-const s1 = prompt("Enter s1:");
+const s1 = prompt("Enter s1:"); 
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
